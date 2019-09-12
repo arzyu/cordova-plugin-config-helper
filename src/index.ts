0ea14a280c3261ec26bf234c329b1170da9b1dbb
arzyu <arzyu@live.cn>
2019-09-12 10:15:13 +0800

    use xml2js to process config.xml

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { resolve } from "path";
 import { readFileSync, writeFileSync } from "fs";
 
+import { js2xml, xml2js, ElementCompact } from "xml-js";
 import render from "es6-template-strings"
 import ip from "ip";
 import { getJson } from "@arzyu/get-json";
@@ -22,10 +23,19 @@ export = ({ opts }: any) => {
       ? render(`${config["DEV_SERVER"]}/${config["INDEX"]}`, { host: ip.address() })
       : config["INDEX"];
 
-    const data = { version: pkgInfo.version, url };
-    const template = readFileSync(resolve(projectRoot, config.TEMPLATE))
+    const template = readFileSync(resolve(projectRoot, config.TEMPLATE), { encoding: "utf8" });
+    const xmlObj: ElementCompact = xml2js(template, { compact: true });
 
-    writeFileSync(resolve(projectRoot, "config.xml"), render(template, data));
+    if (!xmlObj["widget"] || !xmlObj["widget"]._attributes["version"] || !xmlObj["widget"]["content"]) {
+      throw new Error(`Invalid ${config.TEMPLATE}, need '<widget version="\${version}" ...>' and "<content src="\${url}" />`)
+    }
+
+    xmlObj["widget"]._attributes["version"] = pkgInfo.version;
+    xmlObj["widget"]["content"]._attributes["src"] = url;
+
+    const xmlText = js2xml(xmlObj, { compact: true, spaces: 2 });
+
+    writeFileSync(resolve(projectRoot, "config.xml"), xmlText);
   } catch (error) {
     console.error(`[${pluginId}]: ${error.stack}`);
   }
